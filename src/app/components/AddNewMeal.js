e13d399fc757e6f86701349519e62574c10c0776
Jon Tschida <jon.tschida@yahoo.com>
2024-02-29 16:29:36 -0600

    adding meal work

--- src/app/components/AddNewMeal.js
+++ src/app/components/AddNewMeal.js
@@ -2,21 +2,23 @@
 import React from "react";
 import plusSign from "../../../public/images/plusSign.svg";
 import Image from "next/image";
+import { capitalize } from "../scripts/capitalize";
 
 export default function AddNewMeal(props) {
   const { setMealsList } = props;
 
-//   State
+  //   State
   const [addingMeal, setAddingMeal] = React.useState({
     addingTitle: false,
-    addingIngredients: false
+    addingIngredients: false,
   });
-  
+
+  const [addIngredient, setAddIngredient] = React.useState("")
+
   const [mealDetails, setMealDetails] = React.useState({
     mealTitle: "",
-    mealIngredients: [],
-  })
-
+    mealIngredients: [""],
+  });
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -29,54 +31,84 @@ export default function AddNewMeal(props) {
     });
   };
 
-  const handleTitle = () => {
-    setAddingMeal(prevState => {
-        return{
-            ...prevState,
-            addingTitle: true
-        }
-    }) 
+  const handleIngredientChange = (e) =>{
+    setAddIngredient(e.target.value)
   }
 
-  const handleSubmit = (event) => {
+  const handleSetTitle = () => {
+    setAddingMeal((prevState) => {
+      return {
+        ...prevState,
+        addingTitle: true,
+      };
+    });
+  };
+
+  const handleTitleSubmit = (event) => {
+    event.preventDefault();
+
+    setAddingMeal((prevState) => {
+      return {
+        ...prevState,
+        addingIngredients: true,
+      };
+    });
+  };
+
+  const handleIngredientSubmit = (event) =>{
     event.preventDefault()
-    
-    setAddingMeal(prevState=>{
+
+    setMealDetails(prevState => {
         return{
             ...prevState,
-            addingIngredients: true
+            mealIngredients: [...prevState.mealIngredients, capitalize(addIngredient)]
         }
     })
+    setAddIngredient("")
   }
-
   return (
-    <div
-      className={`flex flex-row justify-center items-center w-4/5 h-10 m-auto mt-2 bg-slate-400`}
-    >
-      {addingMeal.addingTitle ? (
-        <form onSubmit={(event)=> handleSubmit(event)}>
-          <input
-            type="text"
-            name="mealTitle"
-            value={mealDetails.mealTitle}
-            placeholder="Meal title"
-            onChange={handleChange}
-            className="text-center"
-          ></input>
-        </form>
-      ) : (
-        <>
-          <h1 className="m-2">Add a meal</h1>
-          <Image
-            priority
-            src={plusSign}
-            alt="add meal image"
-            className="m-2 cursor-pointer select-none"
-            onClick={handleTitle}
-          />
-        </>
+    <>
+      <div
+        className={`flex flex-row justify-center items-center w-4/5 h-10 m-auto mt-2 bg-slate-400`}
+      >
+        {addingMeal.addingTitle ? (
+          <form onSubmit={(event) => handleTitleSubmit(event)}>
+            <input
+              type="text"
+              name="mealTitle"
+              value={mealDetails.mealTitle}
+              placeholder="Meal title"
+              onChange={handleChange}
+              className="text-center"
+            ></input>
+          </form>
+        ) : (
+          <>
+            <h1 className="m-2">Add a meal</h1>
+            <Image
+              priority
+              src={plusSign}
+              alt="add meal image"
+              className="m-2 cursor-pointer select-none"
+              onClick={handleSetTitle}
+            />
+          </>
+        )}
+      </div>
+      {addingMeal.addingIngredients && (
+        <div className="w-4/5 p-2 pl-5 m-auto bg-slate-300">
+          <form onSubmit={handleIngredientSubmit}>
+            <input
+              type="text"
+              placeholder="Add meal ingredient"
+              value={addIngredient}
+              onChange={handleIngredientChange}
+              className="text-center"
+            ></input>
+          </form>
+          {mealDetails.mealIngredients && mealDetails.mealIngredients.map(item => <p>{item}</p>)}
+        </div>
       )}
-      {addingMeal.addingIngredients && <h2>TEST</h2>}
-    </div>
+    </>
   );
 }
