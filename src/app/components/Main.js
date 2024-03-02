c97d34ce7b922f578b8ebe53f8ca5246c9b085bd
Jon Tschida <jon.tschida@yahoo.com>
2024-03-02 14:41:29 -0600

    setup search recipe modal

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -5,29 +5,47 @@ import Meal from "./Meal";
 import GroceryList from "./GroceryList";
 import AddNewMeal from "./AddNewMeal";
 import { deleteItem } from "../scripts/deleteItem";
-import closeIcon from "../../../public/images/closeButton.svg";
+import SearchRecipes from "./SearchRecipes";
 
 export default function Main() {
   const [formInput, setFormInput] = React.useState("");
   const [listIngredients, setListIngredients] = React.useState([]);
   const [mealsList, setMealsList] = React.useState([]);
+  const [searchingRecipes, setSearchingRecipes] = React.useState(false);
+
+  const openCloseSearchRecipes = (setFunction) =>
+    setFunction((prevState) => !prevState);
+
   return (
     <main>
-      <div className="flex items-center justify-around w-4/5 h-[600px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      {searchingRecipes && (
+        <SearchRecipes
+          setSearchingRecipes={setSearchingRecipes}
+          openCloseSearchRecipes={openCloseSearchRecipes}
+        />
+      )}
+      <div className="flex items-center justify-around w-4/5 h-[600px] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
         <div className="w-2/5 p-1 border border-b rounded-md h-4/5 drop-shadow-md bg-slate-100">
           <h1 className="text-center">Meals</h1>
           <hr className="w-3/5 m-auto mb-5" />
           {mealsList.map((meal, index) => (
-            <>
-              <p key={index}>{meal}</p>
-              <Image priority alt="close icon" src={closeIcon} onClick={()=>deleteItem(setMealsList, index)}/>
-            </>
+            <div className="flex flex-row-reverse justify-around">
+              <p key={index} className="flex-grow">
+                {meal}
+              </p>
+            </div>
           ))}
           <AddNewMeal
             setFormInput={setFormInput}
             setMealsList={setMealsList}
             setListIngredients={setListIngredients}
           />
+          <div
+            className="absolute bottom-0 p-2 mb-2 translate-x-1/2 rounded-md cursor-pointer select-none right-1/2 bg-slate-800 text-zinc-100"
+            onClick={() => openCloseSearchRecipes(setSearchingRecipes)}
+          >
+            Search for recipes
+          </div>
         </div>
         <div className="w-2/5 p-1 border border-b rounded-md drop-shadow-md h-4/5 bg-slate-100">
           <h1 className="text-center">Grocery List</h1>
---
+++ src/app/components/SearchRecipes.js
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import closeIcon from "../../../public/images/closeButton.svg";
+import Image from "next/image";
+
+export default function SearchRecipes(props) {
+  const { openCloseSearchRecipes, setSearchingRecipes } = props;
+  return (
+    <div className="w-screen h-screen bg-white/30 backdrop-blur-sm">
+      <div className="absolute w-3/5 -translate-x-1/2 -translate-y-1/2 border border-black h-3/5 left-1/2 top-1/2">
+        <Image
+          priority
+          alt="close"
+          src={closeIcon}
+          className="absolute cursor-pointer select-none top-2 right-2"
+          onClick={() => openCloseSearchRecipes(setSearchingRecipes)}
+        />
+        Search
+      </div>
+    </div>
+  );
+}
