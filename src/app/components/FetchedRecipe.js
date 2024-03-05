93e89703fd8608e8622f909390f7295d4764b9c3
jon-tschida <jon.tschida@yahoo.com>
2024-03-05 12:50:41 -0600

    added images to results

--- src/app/components/FetchedRecipe.js
+++ src/app/components/FetchedRecipe.js
@@ -1,18 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
 export default function FetchedRecipe(props) {
   // Grabbing two things in props, the recipe title, and recipe ingredeints.
   //
-  const { fetchedTitle, fetchedIngredients } = props;
+  const { fetchedTitle, fetchedIngredients, imageURL, imageHeightWidth } = props;
   // console.log(fetchedIngredients)
   return (
     <>
       <h1 className="font-bold text-center">{fetchedTitle}</h1>
+      <Image alt={`${fetchedTitle} image`} src={imageURL} width={imageHeightWidth} height={imageHeightWidth}/>
       <ul className="text-center"> 
         {fetchedIngredients.map((ingredient, index) => {
           return (
             <li key={index}>
-              {ingredient.quantity} {ingredient.measure} {ingredient.food}
+              {ingredient.quantity !== 0 && ingredient.quantity} {ingredient.measure !== "<unit>" && ingredient.measure} {ingredient.food}
             </li>
           );
         })}
--- src/app/components/SearchRecipes.js
+++ src/app/components/SearchRecipes.js
@@ -4,21 +4,21 @@ import closeIcon from "../../../public/images/closeButton.svg";
 import Image from "next/image";
 import axios from "axios";
 import FetchedRecipe from "./FetchedRecipe";
-let testData = require("../scripts/response.json")
-
+let testData = require("../scripts/response.json");
 
 // https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=chicken&app_id=c0d5554f&app_key=4bea9282ec264827a857e5af4390d2ea&imageSize=THUMBNAIL&field=label&field=ingredients
-const apiUrl = 'https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=chicken&app_id=c0d5554f&app_key=4bea9282ec264827a857e5af4390d2ea&imageSize=THUMBNAIL&field=label&field=ingredients';
+const apiUrl =
+  "https://api.edamam.com/api/recipes/v2?type=public&beta=false&q=chicken&app_id=c0d5554f&app_key=4bea9282ec264827a857e5af4390d2ea&imageSize=THUMBNAIL&field=label&field=ingredients";
 
 export default function SearchRecipes(props) {
   const { openCloseSearchRecipes, setSearchingRecipes } = props;
 
   // API request state
-  const [data, setData] = React.useState(null)
-  const [loading, setLoading] = React.useState(false)
-  const [error, setError] = React.useState()
+  const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState();
   // end API request state
-  const [recipeComponents, setRecipeComponents] = React.useState([])
+  const [recipeComponents, setRecipeComponents] = React.useState([]);
 
   /*
   const fetchRecipes = (apiUrl) => {
@@ -42,18 +42,23 @@ export default function SearchRecipes(props) {
   }, [data])
  */
 
-  const displayTestData = (dummyData) =>{
-    let index = 0
-    // console.log(dummyData)
-    // console.log(dummyData.hits[index].recipe)
-
-    // dummyData.hits.map(recipeEntry => console.log(recipeEntry.recipe.ingredients))
-    dummyData.hits.map(recipeEntry => setRecipeComponents(prevState => [...prevState, <FetchedRecipe fetchedTitle={recipeEntry.recipe.label} fetchedIngredients={recipeEntry.recipe.ingredients}/>]))
-  } 
+  const displayTestData = (dummyData) => {
+    dummyData.hits.map((recipeEntry) =>
+      setRecipeComponents((prevState) => [
+        ...prevState,
+        <FetchedRecipe
+          fetchedTitle={recipeEntry.recipe.label}
+          fetchedIngredients={recipeEntry.recipe.ingredients}
+          imageURL={recipeEntry.recipe.images.THUMBNAIL.url}
+          imageHeightWidth={recipeEntry.recipe.images.THUMBNAIL.width}
+        />,
+      ])
+    );
+  };
 
   return (
     <div className="w-screen h-screen bg-white/30 backdrop-blur-sm">
-      <div className="absolute w-3/5 -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg drop-shadow-lg h-3/5 left-1/2 top-1/2">
+      <div className="absolute flex flex-col w-3/5 -translate-x-1/2 -translate-y-1/2 bg-white border border-black rounded-lg drop-shadow-lg h-3/5 left-1/2 top-1/2">
         <Image
           priority
           alt="close"
@@ -61,10 +66,22 @@ export default function SearchRecipes(props) {
           className="absolute cursor-pointer select-none top-2 right-2"
           onClick={() => openCloseSearchRecipes(setSearchingRecipes)}
         />
-        <p className="p-2 rounded-md cursor-pointer select-none bg-slate-300 w-fit" onClick={() => displayTestData(testData)}>search</p>
+        <p
+          className="p-2 rounded-md cursor-pointer select-none bg-slate-300 w-fit"
+          onClick={() => displayTestData(testData)}
+        >
+          Search
+        </p>
         {loading && <p>Loading recipes</p>}
-        <div className="border border-black">
-        {recipeComponents.map((recipeComponent, index) => <div key={index}>{recipeComponent}</div>)}
+        <div className="flex flex-col max-h-full overflow-scroll border border-black">
+          {recipeComponents.map((recipeComponent, index) => (
+            <div
+              key={index}
+              className="flex flex-col p-2 m-2 border border-black w-fit"
+            >
+              {recipeComponent}
+            </div>
+          ))}
         </div>
       </div>
     </div>
