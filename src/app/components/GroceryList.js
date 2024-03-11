df760500c07601c665d1ade2a82c1440bf6469f6
Jon Tschida <jon.tschida@yahoo.com>
2024-03-10 20:50:17 -0500

    added copy text but thats it
    gotta make it a button, and have it do things

--- src/app/components/GroceryList.js
+++ src/app/components/GroceryList.js
@@ -7,18 +7,24 @@ import { deleteItem } from "../scripts/deleteItem";
 export default function GroceryList(props) {
   const { listIngredients, setListIngredients } = props;
   return (
-    <div id="mealAndList" className="w-4/5 p-3 m-auto overflow-y-auto 0 h-4/5">
-      {listIngredients.map((el, index) => (
-        <div className="flex justify-between list-none transition-all hover:scale-105">
-          <li key={index}>{capitalize(el)}</li>
-          <Image
-            priority
-            src={deleteIcon}
-            className="transition-all cursor-pointer hover:-translate-x-1 w-[25px]"
-            onClick={() => deleteItem(setListIngredients, index)}
-          />
-        </div>
-      ))}
-    </div>
+    <>
+      <div
+        id="mealAndList"
+        className="w-4/5 p-3 m-auto overflow-y-auto 0 h-4/5"
+      >
+        {listIngredients.map((el, index) => (
+          <div className="flex justify-between list-none transition-all hover:scale-105">
+            <li key={index}>{capitalize(el)}</li>
+            <Image
+              priority
+              src={deleteIcon}
+              className="transition-all cursor-pointer hover:-translate-x-1 w-[25px]"
+              onClick={() => deleteItem(setListIngredients, index)}
+            />
+          </div>
+        ))}
+      </div>
+      <div>Copy</div>
+    </>
   );
 }
