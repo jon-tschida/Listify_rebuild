f2b3b34e2874ddd7afcfdb79196d6e85c811e8af
Jon Tschida <jon.tschida@yahoo.com>
2024-04-25 11:57:42 -0500

    updated helpcomponent
    added description for what the app is and how to use it.

--- src/app/components/HelpComponent.js
+++ src/app/components/HelpComponent.js
@@ -1,21 +1,42 @@
-import React from 'react'
-import Image from 'next/image'
-import closeIcon from "../../../public/images/closeButton.svg"
+import React from "react";
+import Image from "next/image";
+import closeIcon from "../../../public/images/closeButton.svg";
 
 export default function HelpComponent(props) {
-    const {setOpenHelp} = props
+  const { setOpenHelp } = props;
   return (
-    <div className="w-screen h-screen bg-black/80">
+    <div
+      className="w-screen h-screen bg-black/80"
+    >
       <div className="absolute flex flex-col w-2/5 p-2 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-mealsAndListBg drop-shadow-lg h-3/5 left-1/2 top-1/2">
-      <Image
+        <Image
           priority
           alt="close"
           src={closeIcon}
           className="absolute cursor-pointer select-none top-2 right-2 hover:scale-105"
-          onClick={() => setOpenHelp(prevState => !prevState)}
+          onClick={() => setOpenHelp((prevState) => !prevState)}
         />
-        <h2 className='text-xl text-center'>What is Listify?</h2>
+        <h2 className="text-xl text-center">What is Listify?</h2>
+        <div className="p-4 text-lg">
+          <br />
+          <p>
+            Listify is here to make creating grocery lists easier. Listify does
+            this by adding the ingredients from your meals directly into the
+            grocery list!
+          </p>
+          <br />
+          <p>
+            To get started, create a meal by either using the `Add a meal` form.
+            From here, you can fill out your favorite weekly meals by giving it
+            a title and some ingredients.
+          </p>
+          <br />
+          <p>
+            Need some inspiration? You can search from a whole database of
+            recipes and automatically add any of those meals to your meals list.
+          </p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
