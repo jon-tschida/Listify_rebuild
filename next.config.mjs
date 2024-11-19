58d12cd7741c3812b14a96ac1b04e5111eedb720
Jon Tschida <98907250+jon-tschida@users.noreply.github.com>
2024-11-19 13:29:22 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+/*
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -13,6 +14,37 @@ const nextConfig = {
     env: {
         API_KEY: process.env.API_KEY,
       }
+};
+
+export default nextConfig;
+*/
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'edamam-product-images.s3.amazonaws.com',
+                port: '',
+                pathname: '/web-img/**',
+            },
+        ],
+    },
+    env: {
+        API_KEY: process.env.API_KEY,
+    },
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // Match all routes
+                headers: [
+                    {
+                        key: "Content-Security-Policy",
+                        value: "frame-ancestors: https://app.mutinyhq.com; script-src: https://*.mutinycdn.com; img-src: https://*.mutinycdn.com; connect-src: https://*.mutinyhq.com, https://*.mutinyhq.io, https://*.mutinycdn.com;",
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
